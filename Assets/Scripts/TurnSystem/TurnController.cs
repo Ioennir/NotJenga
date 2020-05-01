8d5907d573196daa6383aa1c8d0f046b758890f6
Fran Núñez <Ioennir@gmail.com>
2020-05-01 04:01:43 +0200

    tomorrow i'll push & complete these things

---
+++ Assets/Scripts/TurnSystem/TurnController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour
+{
+
+#region Public Variables
+#endregion
+
+#region Private Variables
+#endregion
+
+#region Properties
+#endregion
+
+#region MonoBehaviour
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+#endregion
+
+}
---
+++ Assets/Scripts/UIWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWrapper : MonoBehaviour
+{
+
+#region Public Variables
+#endregion
+
+#region Private Variables
+#endregion
+
+#region Properties
+#endregion
+
+#region MonoBehaviour
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+#endregion
+
+}
