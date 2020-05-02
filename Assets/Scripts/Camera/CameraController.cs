6fb70b30321936d0fc7d0cb3f32d1c715ab983fa
danigmx <dgraciamachado99@gmail.com>
2020-05-02 20:25:11 +0200

    camara mejorada, ahora rota en vertical y se mueve sin trompicones

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -20,6 +21,9 @@ public class CameraController : MonoBehaviour
 
     private Camera _c;
     private CylindricCoordinates _cc;
+    private int goUp = 0;
+    private int goDown = 0;
+    private Vector2 inputs = Vector2.zero;
 
     #endregion
 
@@ -27,6 +31,8 @@ public class CameraController : MonoBehaviour
 
     private void Start()
     {
+        target = FindObjectOfType<Tower>().transform;
+        
         _c = GetComponent<Camera>();
         _cc = new CylindricCoordinates(
             target.position, 
@@ -40,12 +46,14 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        // Retrieve Delta time
         float dt = Time.deltaTime;
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-        Vector2 inputs = new Vector2(xInput, yInput);
+
+        // Retrieve inputs
+        RetrieveInputs();
         
-        UpdateCylindricalCoordinates(dt, inputs);
+        // Update camera position data
+        UpdateCylindricalCoordinates(dt,goUp, goDown, inputs);
 
         transform.position = target.position + _cc.toCarthesian();
         transform.rotation = UpdateCameraRotation();
@@ -61,24 +69,41 @@ public class CameraController : MonoBehaviour
 
     #region Private Methods
 
+    private void RetrieveInputs()
+    {
+        // Vertical movement of camera
+        goUp = Input.GetKey(KeyCode.LeftShift) ? 1 : 0;
+        goDown = Input.GetKey(KeyCode.LeftControl) ? -1 : 0;
+        
+        // Horizontal position and vertical rotation of camera
+        inputs.x = Input.GetAxisRaw("Horizontal");
+        inputs.y = Input.GetAxisRaw("Vertical");
+    }
+
     private void ResetCameraPosition()
     {
         _cc.Angle = 0f;
         _cc.Height = 0f;
     }
 
-    private void UpdateCylindricalCoordinates(float delta, Vector2 inputs)
+    private void UpdateCylindricalCoordinates(float delta, int upwards, int downwards, Vector2 axes)
     {
-        float horSpeed = (rotationSpeed * inputs.x) * delta;
-        float verSpeed = (verticalSpeed * inputs.y) * delta;
+        // Get rotation and vertical speed values
+        float rotSpeed = (rotationSpeed * axes.x) * delta;
+        float verSpeed = verticalSpeed * (upwards + downwards) * delta;
 
-        _cc.Angle += horSpeed;
+        // Apply changes to Cylindrical Coordinates
+        _cc.Angle += rotSpeed;
         _cc.Height += verSpeed;
+        
+        // Update offset so we don't need to update rotation
+        offset.y += (verticalSpeed * axes.y) * delta;
+        offset.y = Mathf.Clamp(offset.y, 0.0f, 10.0f);
     }
 
     private Quaternion UpdateCameraRotation()
     {
-        Quaternion newRot = Quaternion.LookRotation(-transform.position, Vector3.up);
+        Quaternion newRot = Quaternion.LookRotation(-transform.position + offset, Vector3.up);
         return newRot;
     }
 
