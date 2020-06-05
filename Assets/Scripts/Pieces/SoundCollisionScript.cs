2894a7e5608dea5c9f68e63827b8cebd7f6e7084
gabivlj <gabitriqui@gmail.com>
2020-06-05 18:48:47 +0100

    changes

--- Assets/Scripts/Pieces/SoundCollisionScript.cs
+++ Assets/Scripts/Pieces/SoundCollisionScript.cs
@@ -9,21 +9,6 @@ public class SoundCollisionScript : MonoBehaviour
 
     public void Start()
     {
-<<<<<<< HEAD
-        controller = GameObject.Find("AudioController").GetComponent<AudioController>();
-        sound = this.GetComponent<Sound>();
-        sound.Init();
-    }
-    public void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Selectable"))
-        {
-            float volume = Mathf.Clamp01(other.relativeVelocity.magnitude / 20);
-            sound.volume = volume;
-            sound.UpdateSource();
-            sound.Play();
-
-=======
         _controller = FindObjectOfType<AudioController>();
     }
 
@@ -43,7 +28,6 @@ public class SoundCollisionScript : MonoBehaviour
                 sound1.volume = volume;
                 sound1.transform.position = transform.position;
             });
->>>>>>> edfee153ddea4586b206d637b44b0638dd370335
         }
     }
    
